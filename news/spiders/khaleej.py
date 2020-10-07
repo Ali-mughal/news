b8d1b68315c0711c32c57d891d88ab2e5c18712b
Ali Mughal <alie.mughal@gmail.com>
2020-10-07 19:31:51 +0400

    schedular done in main.py

--- news/spiders/khaleej.py
+++ news/spiders/khaleej.py
@@ -11,18 +11,30 @@ class KhaleejSpider(Spider):
 
     def parse(self,response):
         all_urls = response.css('a::attr(href)').getall()
-        urls_list = []
+        urls_list1 = []
+        urls_list2 = []
         for all_url in all_urls:
             if  'javascript' not in all_url:
                 absolute_url=response.urljoin(all_url)
-                # print(absolute_url)
-                urls_list.append(absolute_url)
-        print(urls_list)
-    # schedule.every(10).seconds.do(parse)
-    # while True:
-    #     schedule.run_pending()
-    #     time.sleep(1)
+                print(absolute_url)
+                urls_list1.append(absolute_url)
+                urls_list2.append(absolute_url)
+                # yield scrapy.Request(url=url, callback=self.parse_details)
+        print ("getting the length of list:",len(urls_list1))
+        diff=set(urls_list1) & set(urls_list2)
+        print('lsdjflsadf;l###################################################',len(diff))
+        if urls_list1==urls_list2:
+            print('both list are equal')
+
+        # def parse_details(self,response):
+        # yield{
+        #         'name': response.css('h3.author-title::text').get(),
+        #         }
+        
+        
     
         # yield{
         #     'absolute_allurls': [response.urljoin for all_url in all_urls if 'javascript' not in all_url]
         # }
+        ## pagination link
+        
